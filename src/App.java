cd4802e0322062268ae230dfeceab1a7adecb674
Arie Hendrix <naalix@yahoo.com>
2021-10-05 17:16:09 -0700

    Adding assignment code

---
+++ src/App.java
@@ -0,0 +1,20 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Week 3 coding assignment
+		
+		//Section dedicated to declaring strings for the first, middle, and last name of an person
+		String firstName = "Betsy";
+		String middleInitial = " Q. ";
+		String lastName = "Ross";
+		
+		//Concatenation of the strings to form a complete name
+		String fullName = firstName + middleInitial + lastName;
+		
+		//Using the system to print out the complete name along with a greeting and introduction!
+		System.out.println("Hi, my full name is " + fullName + '!');
+	}
+
+}
--- src/assignment.java
+++ src/assignment.java
@@ -4,6 +4,9 @@ public class assignment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
 	}
 
 }
